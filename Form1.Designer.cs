aaa41bb9754654452bf9cacf469d81c722e66beb
Maxim Shabanov <99maxwolf99@gmail.com>
2023-06-02 20:28:50 +0300

    ekzam

---
+++ Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace NetworkEkzam
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.UrlTextBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(730, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "URL";
+            // 
+            // UrlTextBox
+            // 
+            this.UrlTextBox.Location = new System.Drawing.Point(12, 12);
+            this.UrlTextBox.Name = "UrlTextBox";
+            this.UrlTextBox.Size = new System.Drawing.Size(712, 20);
+            this.UrlTextBox.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(713, 415);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Download";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(14, 413);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(209, 23);
+            this.progressBar1.TabIndex = 5;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 39);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(712, 368);
+            this.listBox1.TabIndex = 6;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(229, 413);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(107, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Pause Download";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(342, 413);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(101, 23);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Start Download";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(449, 413);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(105, 23);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Cancel Download";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.UrlTextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox UrlTextBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace NetworkEkzam
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            UrlTextBox.Text = @"https://w.forfun.com/fetch/9d/9db2d4683d92f5f2045e9142fbd82633.jpeg";
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                Downloader downloader = new Downloader(folderBrowserDialog1.SelectedPath + UrlTextBox.Text.Substring(UrlTextBox.Text.LastIndexOf("/")));
+
+                progressBar1.Maximum = (int)GetFileSize(UrlTextBox.Text);
+                MessageBox.Show(progressBar1.Maximum.ToString());
+
+                listBox1.Items.Add(downloader.name);
+                button2.Click += downloader.Reset;
+                button4.Click += downloader.Set;
+                button3.Click += downloader.Cancel;
+
+                downloader.client = new HttpClient();
+                downloader.stream = await downloader.client.GetStreamAsync(UrlTextBox.Text);
+
+                await Task.Run(()=> Download(downloader));
+            }
+        }
+
+        async Task Download(Downloader downloader)
+        {
+            try
+            {
+                byte[] bytes = new byte[progressBar1.Maximum];
+                using (FileStream fs = new FileStream(downloader.path, FileMode.Create))
+                {
+                    int size = await downloader.stream.ReadAsync(bytes, 0, bytes.Length, downloader.token);
+                    while (size > 0)
+                    {
+                        await Task.Yield();
+                        downloader.m_.WaitOne();
+                        BeginInvoke(new Action(() =>
+                        {
+                            progressBar1.Value += size;
+                        }));
+                        await fs.WriteAsync(bytes, 0, size);
+                        size = await downloader.stream.ReadAsync(bytes, 0, bytes.Length, downloader.token);
+                    }
+                }
+                BeginInvoke(new Action(() =>
+                {
+                    progressBar1.Value = 0;
+                }));
+                MessageBox.Show("Done!");
+            }
+            catch (Exception ex) {}
+        }
+
+        public long GetFileSize(string url)
+        {
+            long result = -1;
+            System.Net.WebRequest req = System.Net.WebRequest.Create(url);
+            req.Method = "HEAD";
+            using (System.Net.WebResponse resp = req.GetResponse())
+            {
+                if (long.TryParse(resp.Headers.Get("Content-Length"), out long ContentLength))
+                {
+                    result = ContentLength;
+                }
+            }
+            return result;
+        }
+    }
+
+    class Downloader
+    {
+        
+        public string path;
+        public string name;
+        public ManualResetEvent m_ = new ManualResetEvent(true);
+        CancellationTokenSource cancelTokenSource = new CancellationTokenSource();
+        public CancellationToken token;
+
+        public HttpClient client = new HttpClient();
+        public Stream stream;
+
+
+        public Downloader(string path)
+        {
+            this.path = path;
+            name = path.Substring(path.LastIndexOf("/"));
+            token = cancelTokenSource.Token;
+        }
+
+        public void Reset(object sender, EventArgs e) // button 2
+        {
+            m_.Reset();
+        }
+
+        public void Set(object sender, EventArgs e) // button 4
+        {
+            m_.Set();
+        }
+
+        public void Cancel(object sender, EventArgs e) // button 3
+        {
+            cancelTokenSource.Cancel();
+        }
+    }
+}
